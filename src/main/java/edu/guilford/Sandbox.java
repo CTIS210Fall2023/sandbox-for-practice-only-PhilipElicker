89475b2fc7193da73665516e7586da0be0814156
Philip Elicker <pelicker@guilford.edu>
2023-09-07 09:20:59 -0400

    Complete

--- src/main/java/edu/guilford/Sandbox.java
+++ src/main/java/edu/guilford/Sandbox.java
@@ -1,8 +1,21 @@
 package edu.guilford;
 
+import java.util.Timer;
+
+import javax.swing.plaf.synth.SynthStyle;
+
 public class Sandbox {
     public static void main(String[] args) {
-        // A place for testing Java code
+    int Value1 = 140;
+    int Value2 = 200;
+    
+    double Value3 = Value1 + Value2;
+    System.out.println("Hello " + Value3);
+    if (Value1 > Value2) {
+        System.out.println("Value 1 is greator");
+    } else if (Value2 > Value1) {
+        System.out.println("Value 2 is greator");
+    }
         
     }
 }
